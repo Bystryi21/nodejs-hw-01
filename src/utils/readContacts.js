740e1937e2fbcb6e7580177475d7c752c734ce56
Denys Bystryi <bystryydenis211999@gmail.com>
2024-10-19 15:12:56 +0300

    added first commit

--- src/utils/readContacts.js
+++ src/utils/readContacts.js
@@ -1,3 +1,9 @@
+import { readFile } from 'node:fs/promises';
+
 import { PATH_DB } from '../constants/contacts.js';
 
-export const readContacts = async () => {};
+export const readContacts = async () => {
+  const data = await readFile(PATH_DB, 'utf-8');
+  console.log(data);
+  return JSON.parse(data);
+};
